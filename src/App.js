04f7bb178d3135433653806f20a12efb35537d0c
go2023 <tetyanago2018@gmail.com>
2024-02-25 16:57:33 -0800

    Functionality was implemented

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import ScrollToSection from "./components/scroll-to-section";
 
 function App() {
   return (
     <div className="App">
-
+      {/* Scroll to a Particular Section */}
+      <ScrollToSection/>
     </div>
   );
 }
